d87ef6d888a8e408df80ded6a07d57916b410fa4
Shin-Taro <sh.takahashi.113@gmail.com>
2023-12-04 11:48:16 +0900

    refactor: adjust sleep time

--- src/domain/user/modules/isLoggedIn/index.ts
+++ src/domain/user/modules/isLoggedIn/index.ts
@@ -3,7 +3,7 @@ import { sleep } from "@shared/modules/sleep"
 import type { AstroCookies } from "astro"
 
 export const isLoggedIn = async (cookies: AstroCookies): Promise<boolean> => {
-  await sleep(1500)
+  await sleep(500) // Simulate a network request
   const user = cookies.get("user")
 
   return Boolean(user)
