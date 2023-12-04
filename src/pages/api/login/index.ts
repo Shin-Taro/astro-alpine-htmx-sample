aa692f35b4b4133f6fc145e0f963f269cf1fb40d
Shin-Taro <sh.takahashi.113@gmail.com>
2023-12-04 23:08:19 +0900

    refactor: fix login api validation condition

--- src/pages/api/login/index.ts
+++ src/pages/api/login/index.ts
@@ -9,6 +9,6 @@ export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData()
   const email = data.get("email")
   const password = data.get("password")
-  const isValid = !email || !password
-  return isValid ? new Response(ErrorDialog) : new Response(SuccessDialog, { headers: { Location: "/mypage", "Set-Cookie": "user=dummy-user; Path=/;" } })
+  const isValid = Boolean(email) && Boolean(password)
+  return isValid ? new Response(SuccessDialog, { headers: { Location: "/mypage", "Set-Cookie": "user=dummy-user; Path=/;" } }) : new Response(ErrorDialog)
 }
