9d4d567201fb46832b3e32e36138464f8e3939a3
Shin-Taro <sh.takahashi.113@gmail.com>
2023-12-04 21:02:18 +0900

    feat: adjust login api

--- src/pages/api/login/index.ts
+++ src/pages/api/login/index.ts
@@ -3,13 +3,12 @@ import type { APIRoute } from "astro"
 import fs from "node:fs"
 
 const ErrorDialog = fs.readFileSync("src/domain/user/components/features/LoginForm/components/elements/ErrorDialog/index.html", "utf8")
+const SuccessDialog = fs.readFileSync("src/domain/user/components/features/LoginForm/components/elements/SuccessDialog/index.html", "utf8")
 
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData()
   const email = data.get("email")
   const password = data.get("password")
-  if (!email || !password) {
-    return new Response(ErrorDialog)
-  }
-  return new Response("Success!", { headers: { location: "/mypage" } })
+  const isValid = !email || !password
+  return isValid ? new Response(ErrorDialog) : new Response(SuccessDialog, { headers: { Location: "/mypage", "Set-Cookie": "user=dummy-user; Path=/;" } })
 }
